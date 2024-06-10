e39c34ab91dae8aaf1e82e4a17c34e3e6bd56c06
emmimelon <emmimelon@gmail.com>
2024-06-10 15:35:58 -0400

    created component w/ pdf upload button and extracts text to console log

---
+++ clerk-react/src/components/PDFExtract.jsx
@@ -0,0 +1,20 @@
+import pdfToText from "react-pdftotext";
+
+function extractText(event) {
+    const file = event.target.files[0]
+    pdfToText(file)
+        .then(text => console.log(text))
+        .catch(error => console.error("Failed to extract text from pdf"))
+}
+
+function PDFExtract() {
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <input type="file" accept="application/pdf" onChange={extractText}/>
+            </header>
+        </div>
+    );
+}
+export default PDFExtract;
