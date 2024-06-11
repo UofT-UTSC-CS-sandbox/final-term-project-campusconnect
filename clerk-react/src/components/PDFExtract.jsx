af51b59fe4fbdf107bc76e74afc58de236ae1b77
emmimelon <emmimelon@gmail.com>
2024-06-11 17:46:54 -0400

    temp edits to PDFExtract

--- clerk-react/src/components/PDFExtract.jsx
+++ clerk-react/src/components/PDFExtract.jsx
@@ -1,16 +1,29 @@
 import pdfToText from "react-pdftotext";
 
-function extractText(event) {
+/*function extractText(event) {
     const file = event.target.files[0]
     pdfToText(file)
         .then(text => console.log(text))
         .catch(error => console.error("Failed to extract text from pdf"))
+}*/
+
+function extractText(event) {
+    const file = event.target.files[0]
+    pdfToText(file)
+        .then(text => 
+            parsetext(text)
+        )
+        .catch(error => console.error("Failed to extract text from pdf"))
+}
+
+function parsetext(text){
+    console.log(text)
 }
 
 function PDFExtract() {
 
     return (
-        <div className="App">
+        <div>
             <header className="App-header">
                 <input type="file" accept="application/pdf" onChange={extractText}/>
             </header>
