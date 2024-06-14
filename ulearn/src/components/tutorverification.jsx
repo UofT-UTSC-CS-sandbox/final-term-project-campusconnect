741c2c08255adabf345496b25bac56c34a14719b
emmimelon <emmimelon@gmail.com>
2024-06-13 21:43:13 -0400

    bugfix: fails invalid transcripts

--- ulearn/src/components/tutorverification.jsx
+++ ulearn/src/components/tutorverification.jsx
@@ -5,11 +5,13 @@ function getCourseGrade(course, text){
     let courseindex = text.search(course)
     if (courseindex == -1){
         console.error("Could not find course in transcript")
+        return -1;
     }
     let smalltext = text.substring(courseindex)
     let creditindex = smalltext.search("0.50")
     if (creditindex == -1){
         console.error("Could not find credit in transcript")
+        return -1;
     }
     let smallertext = smalltext.substring(creditindex)
     let finalgrade = smallertext.slice(7,10);
@@ -25,7 +27,7 @@ function VerifyTutor(courses, text){
         let grade = getCourseGrade(courses[i], text);
         if (grade < 80){
             pass = 0;
-            toast.error("You are not elligible to teach " + courses[i] + ". Please remove course from selection.");
+            toast.error("Invalid transcript or you are not elligible to teach " + courses[i] + ". Please remove course from selection.");
         }
     }
     return(
