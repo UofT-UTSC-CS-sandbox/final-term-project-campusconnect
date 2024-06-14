3d225c4518d378f7ebcd408565f54f47c0ad1e21
AarushiDoshi23 <aarushidoshi23@gmail.com>
2024-06-14 03:08:53 -0400

    Added PersonalInfo, WhoAreYou Page and Main Screen

--- ulearn/src/main.jsx
+++ ulearn/src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import {Route, Routes, BrowserRouter} from 'react-router-dom'
 import './index.css'
 import { ClerkProvider } from '@clerk/clerk-react'
-
-
-// Import your publishable key
+import PersonalInfoPage from './Pages/PersonalInfo/PersonalInfoPage.jsx'
+import WhoAreYouPage from './Pages/WhoAreYou/whoAreYouPage.jsx'
+import App from './Pages/App.jsx'
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
 if (!PUBLISHABLE_KEY) {
@@ -15,7 +15,13 @@ if (!PUBLISHABLE_KEY) {
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-      <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App/>} />
+        <Route path="/personalInfo" element={<PersonalInfoPage/>} />
+        <Route path="/whoAreYou" element={<WhoAreYouPage/>} />
+      </Routes>
+    </BrowserRouter>
     </ClerkProvider>
   </React.StrictMode>,
 )
