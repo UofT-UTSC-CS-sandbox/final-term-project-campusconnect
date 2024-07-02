09fd3fdacb1bd0d46faf27d056667dc60ca76d0e
arinaazmi <88069395+arinaazmi@users.noreply.github.com>
2024-07-01 23:59:44 -0400

    Feat: home page (#15)
    * feat: basic ui of homepage
    
    * bug fixes: updates and fixes for homepage and imageupload
    
    * update: ui update
    
    * feat: fixed image upload bug, added language filter, fixed ui of courses displayed

--- ulearn/server/index.js
+++ ulearn/server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const UserModel = require('./models/User');
+const path = require('path'); // Import the path module
 const cors = require('cors');
 const TutorModel = require('./models/Tutor');
 
@@ -16,10 +17,6 @@ mongoose.connect("mongodb+srv://ulearncc:aammmulearn@ulearn.jjcv6kg.mongodb.net/
 
 app.post(('/login'), (req, res) => {
     const {email} = req.body;
-    const {image} = req.body;
-    const {clerkId} = req.body;
-    const {name} = req.body;
-
     UserModel.findOne({email: email})
     .then(user => {
         if(user) {
@@ -31,27 +28,13 @@ app.post(('/login'), (req, res) => {
     })
 });
 
-app.post(('/update'), (req, res) => {
-    const { clerkId, email, name, image } = req.body;
-
-    UserModel.findOneAndUpdate(
-      { email: email },
-      { clerkId, name, image },
-      { new: true, upsert: true } // upsert: true will create a new document if no document matches the query
-    )
-      .then(user => res.json(user), console.log("User found successfully"))
-      .catch(err => {
-        console.error("Error during registration:", err);
-        res.status(500).json({ error: "Internal server error" });
-      });
-});
-
+// update user info
 app.post(('/updatePersonalInfo'), (req, res) => {
-    const { clerkId, email, name, university, year, languages } = req.body;
+    const { clerkId, email, name, university, year, languages, image } = req.body;
 
     UserModel.findOneAndUpdate(
       { email: email },
-      { clerkId, name, university, year, languages},
+      { clerkId, name, university, year, languages, image},
       { new: true, upsert: true } // upsert: true will create a new document if no document matches the query
     )
       .then(user => res.json(user), console.log("User found successfully"))
@@ -74,6 +57,32 @@ app.post('/register', (req, res) => {
     .catch(err => res.json(err))
     
 })
+
+// fetches all tutors from tutor collection
+app.get('/gettutors', async (req, res) => {
+  try {
+    const tutors = await TutorModel.find();
+    res.json(tutors);
+  } catch (error) {
+    res.status(500).json({ message: 'Error fetching tutors' });
+  }
+});
+
+// fetches users by email specified in query
+app.get('/getUserByEmail', async (req, res) => {
+  const { email } = req.query; // Use req.query to extract email from query parameters
+  try {
+    const user = await UserModel.findOne({ email: email });
+    if (user) {
+      res.json(user);
+    } else {
+      res.status(404).json({ message: 'User not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'Error fetching user or not found' });
+  }
+});
+
 
 app.listen("3001", () => {
     console.log(`Server started on port 3001`);
