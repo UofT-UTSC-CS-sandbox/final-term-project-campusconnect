08b5fc7f0289f47466fd786f2bae0fb7ba0887eb
emmimelon <emmimelon@gmail.com>
2024-07-03 17:22:57 -0400

    bug: fixed stream api key failure issue

--- ULearn/src/Pages/Chatrooms/chatroom.jsx
+++ ULearn/src/Pages/Chatrooms/chatroom.jsx
@@ -12,51 +12,32 @@ import {
 } from 'stream-chat-react';
 import './chatroom_layout.css';
 import 'stream-chat-react/dist/css/v2/index.css';
-import { useUser } from '@clerk/clerk-react';
+import { useUser } from "@clerk/clerk-react";
+import axios from 'axios';
 
-const apiKey = 'g8evherw6njt';
+//const apiKey = 'g8evherw6njt';
 //const apiSecret = '2x2rezwpctxjeuvu65vt5hxwtzg84ve6zhnyfbt5e7bwd7h4emckuavq28ghph6p';
-const userId = 'ulearn4';
+//const userId = 'ulearn4';
 // Assume this token is securely fetched from your server
 //const serverClient = StreamChat.getInstance( apiKey, apiSecret);
 // Create User Token
 //const userToken = serverClient.createToken(userId);
 //console.log(userToken);
 
-const filters = { members: { $in: [userId] }, type: 'messaging' };
-const options = { presence: true, state: true };
-const sort = { last_message_at: -1 };
 
-/*//new miri temp
-const chatClient = new StreamChat(apiKey)
-//const userToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoidWxlYXJuNCIsImV4cCI6MTcyMDE5NTg1OH0.0pPNRVFlNMQ-kdJDBUzwv69iqPEyLPWURJp79eGykQ0"
+const apiKey = import.meta.env.VITE_STREAM_VIDEO_API_KEY;
 
-axios.post("/getChatroomToken", async (req, res) => {
-    const { user_id } = req.body;
-    try {
-      const token = await chatClient.createToken(user_id.toString());
-      res.status(200).json({
-        payload: token,
-      });
-    } catch (error) {
-      console.log(error);
-      res.sendStatus(400);
-    }
-  });
 
-const set = await chatClient.connectUser({id: userID}, token);
-
-const channel = client.channel('messaging', {
-    members: ['ulearn4', 'user2'],
-});
-await channel.create();
-//*/
-
-const chatRoom = () => {
+const ChatRoom = () => {
   const [client, setClient] = useState(null);
-
+  const { isLoaded } = useUser(null);
+  const {user} = useUser();
+  //console.log(user.id);
+  console.log('hello');
   useEffect(() => {
-    const initChat = async () => {
+    if (!isLoaded || !user) return;
+    
+    /*const initChat = async () => {
       if (!client){
         const chatClient = StreamChat.getInstance(apiKey);
         
@@ -73,19 +54,43 @@ const chatRoom = () => {
     };
   
       initChat();
-      // return () => {
-      //   if (client) client.disconnectUser();
-      // };
-   
+      
+  }, []);*/
+  console.log(apiKey);
+  if (!apiKey) {
+    console.error("Missing API Key");
+    return;
+  }
 
+  axios.post('http://localhost:3001/getChatroomToken', user).then(response => {      
+    const token = response.data;
+    const client = new StreamChat({
+      apiKey,
+      user: {
+        id: user.id,
+        name: user.fullName,
+        image: user.imageUrl,
+      },
+      token,
+    });
+    client.connectUser({ id: user.id }, token);
+    setClient(client);
+  }).catch(error => {
+    console.error("Failed to get user token:", error);
+  });
+}, [user, isLoaded]);
+//console.log(user.id);
+//const filters = { members: { $in: [user.id] }, type: 'messaging' };
+// const options = { presence: true, state: true };
+// const sort = { last_message_at: -1 };
+      
     
-  }, []);
 
   if (!client) return <div>Loading...</div>;
 
   return (
     <Chat client={client}>
-      <ChannelList filters={filters} options={options} sort={sort} />
+      <ChannelList /*filters={filters} options={options} sort={sort}*/ />
       <Channel>
         <Window>
           <ChannelHeader />
@@ -98,4 +103,4 @@ const chatRoom = () => {
   );
 };
 
-export default chatRoom;
+export default ChatRoom;
