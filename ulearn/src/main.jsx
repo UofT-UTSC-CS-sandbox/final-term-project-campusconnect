52faae5634e854206b702294bc6083952fb385a1
AarushiDoshi23 <aarushidoshi23@gmail.com>
2024-07-04 12:33:24 -0400

    Refactored App UI

--- ulearn/src/main.jsx
+++ ulearn/src/main.jsx
@@ -4,7 +4,6 @@ import {Route, Routes, BrowserRouter} from 'react-router-dom'
 import './index.css'
 import { ClerkProvider } from '@clerk/clerk-react'
 import PersonalInfoPage from './Pages/PersonalInfo/PersonalInfoPage.jsx'
-import WhoAreYouPage from './Pages/WhoAreYou/whoAreYouPage.jsx'
 import App from './Pages/App.jsx'
 import TutorPage from './Pages/TutorInfo/tutorInfoPage.jsx'
 import HomePage from './Pages/Home/homepage.jsx'
@@ -22,7 +21,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/" element={<App/>} />
         <Route path="/personalInfo" element={<PersonalInfoPage/>} />
-        <Route path="/whoAreYou" element={<WhoAreYouPage/>} />
         <Route path="/tutorInfo" element={<TutorPage />}/>
         <Route path="/homePage" element={<HomePage />} />
       </Routes>
