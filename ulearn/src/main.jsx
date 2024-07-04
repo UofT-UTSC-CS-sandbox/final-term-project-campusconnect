9966e380b1753db0f50cae38687241acc071906a
Miri <117676677+emmimelon@users.noreply.github.com>
2024-07-04 16:21:50 -0400

    Feat: chatroom (#19)
    * feat: creating chatroom page file
    
    * feat: chatroom
    
    * feat: added getting chatroom token in index.js
    
    * bug: fixed stream api key failure issue
    
    * fix: server-side token generation
    
    * feat: #10 passed tutor id information from homepage to chatroom using message button in tutor profile
    
    * feat: #10 implemented chatroom creation with message button
    
    * chore: minor changes to chat, and cleanup of files after merge conflicts
    
    ---------
    
    Co-authored-by: anushak18 <karkhanis.anusha@gmail.com>

--- ulearn/src/main.jsx
+++ ulearn/src/main.jsx
@@ -8,6 +8,8 @@ import PersonalInfoPage from './Pages/PersonalInfo/PersonalInfoPage.jsx'
 import App from './Pages/App.jsx'
 import TutorPage from './Pages/TutorInfo/tutorInfoPage.jsx'
 import HomePage from './Pages/Home/homepage.jsx'
+import ChatRoom from './Pages/Chatrooms/chatroom.jsx'
+import TutorProfile from './Pages/Home/tutorProfile.jsx'
 import StreamVideoCallProvider from './Pages/StreamVideoCallProvider.jsx'
 import Meeting from './Pages/Meeting/Meeting.jsx'
 
@@ -21,17 +23,19 @@ if (!PUBLISHABLE_KEY) {
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-      <StreamVideoCallProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<App />} />
-            <Route path="/personalInfo" element={<PersonalInfoPage />} />
-            <Route path="/tutorInfo" element={<TutorPage />} />
-            <Route path="/homePage" element={<HomePage />} />
-            <Route path="/meeting/:id" element={<Meeting />} />
-          </Routes>
-        </BrowserRouter>
-      </StreamVideoCallProvider>
+    <StreamVideoCallProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App/>} />
+          <Route path="/personalInfo" element={<PersonalInfoPage/>} />
+          <Route path="/tutorInfo" element={<TutorPage />}/>
+          <Route path="/homePage" element={<HomePage />} />
+          <Route path="/tutor/:name" element={<TutorProfile />} />
+          <Route path="/chatRoom" element={<ChatRoom />} />
+          <Route path="/meeting/:id" element={<Meeting />} />
+        </Routes>
+      </BrowserRouter>
+    </StreamVideoCallProvider>
     </ClerkProvider>
   </React.StrictMode>,
 )
