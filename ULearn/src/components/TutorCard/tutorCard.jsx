b044c57c3a93176b0231c48f7a6b01419a00240f
AarushiDoshi23 <aarushidoshi23@gmail.com>
2024-07-05 11:33:25 -0400

    Added tutor search functionality for homepage

---
+++ ULearn/src/components/TutorCard/tutorCard.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './tutorCard.css';
+
+const TutorCard = ({email, tutor, handleTutorClick, formatCourses }) => {
+    return (
+        <div
+            key={tutor.name}
+            className="tutor-card"
+            onClick={() => handleTutorClick(tutor.name, email)}
+        >
+            <img src={tutor.image} alt={tutor.name} className="tutor-card-tutor-image" />
+            <div className="tutor-card-tutor-info">
+                <p className="tutor-card-tutor-name">{tutor.name}</p>
+                <div className="tutor-card-tutor-rating">
+                    {'★'.repeat(tutor.rating)}{'☆'.repeat(5 - tutor.rating)}
+                </div>
+                <p> Price: ${tutor.price}/hr</p>
+                <p>{formatCourses(tutor.courses)}</p>
+            </div>
+        </div>
+    );
+};
+
+export default TutorCard;
--- ulearn/src/Pages/PersonalInfo/PersonalInfoPage.jsx
+++ ulearn/src/Pages/PersonalInfo/PersonalInfoPage.jsx
@@ -5,7 +5,6 @@ import './PersonalInfoPage.css'
 import { universities, years, languages } from './constants.jsx';
 import axios from 'axios';
 
-
 const pipCustomStyles = {
     control: (provided, state) => ({
         ...provided,
@@ -84,9 +83,6 @@ const PersonalInfoPage = () => {
                         .then(response => {
                         if (response.data === "found") {
                             axios.post(`http://localhost:3001/update`, { clerkId, email, name, image })
-                            .then(response => {
-                                console.log(response.data);
-                            });
                         }
                         });
                     }
@@ -115,18 +111,27 @@ const PersonalInfoPage = () => {
             let languages = selectedLanguages.map(lang => lang.value)
             const university = selectedUniversity.value;
             const year = selectedYear.value;
+            const image = user.imageUrl;
+            let finishedSignUp = false;
             axios.post(`http://localhost:3001/login`, { email })
                         .then(response => {
-                        if (response.data === "found") {
-                            axios.post(`http://localhost:3001/updatePersonalInfo`, { clerkId, email, name, university, year, languages })
-                            .then(response => {
-                                if (selectedRole.value === 'Tutor') { 
+                        if (response.data !== "not found") {
+                            if (selectedRole.value === 'Tutor') {
+                                axios.post(`http://localhost:3001/updatePersonalInfo`, { clerkId, email, name, university, year, languages, image, finishedSignUp})
+                                .then(() => {
                                     window.location.href = "/tutorInfo"
-                                }
-                                else {
+                                });  
+                            }
+                            else {
+                                finishedSignUp = true;
+                                axios.post(`http://localhost:3001/updatePersonalInfo`, { clerkId, email, name, university, year, languages, image, finishedSignUp})
+                                .then(() => {
                                     window.location.href = "/homePage"
-                                }
-                            });
+                                }); 
+                                
+                            }
+
+                            
                         }
                         });
         }
@@ -135,7 +140,7 @@ const PersonalInfoPage = () => {
         }
     }
     return (
-        <div>
+        <div className='pip-body'>
             <div className='pip-background-overlay'></div>
             <h2 className='pip-title'>Register Account</h2>
             <div className='pip-wrapper'>
