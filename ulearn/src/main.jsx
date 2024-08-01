3c7e97ef03450dbc9300f63a2611ea0e7a8fac29
Aarushi Doshi <aarushidoshi23@gmail.com>
2024-08-01 16:24:31 -0400

    Feat-tutorCalendar (#43)
    * feat: basic tutor calendar setup
    
    * feat: backend connected, availability is now stored
    
    * feat-tutorCalendar SCRUM-19 added immutable
    * feat-tutorCalendar SCRUM-23 
    calendar on tutor profile page for students to view
    
    ---------
    
    Co-authored-by: arinaazmi <arina.cat2003@gmail.com>
    Co-authored-by: arinaazmi <88069395+arinaazmi@users.noreply.github.com>

--- ulearn/src/main.jsx
+++ ulearn/src/main.jsx
@@ -15,6 +15,7 @@ import Meeting from './Pages/Meeting/Meeting.jsx'
 import Recordings from './Pages/Recordings/Recordings.jsx'
 import Feedback from './Pages/Feedback/Feedback.jsx'
 import EditTutorProfile from './Pages/Home/editTutorProfile.jsx'
+import Calendar from './Pages/Calendar/Calendar.jsx'
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
@@ -38,6 +39,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
           <Route path="/recordings" element={<Recordings />} />
           <Route path="/Feedback" element={<Feedback />} />
           <Route path="/editTutorProfile" element={<EditTutorProfile />} />
+          <Route path="/calendar" element={<Calendar />} />
+
         </Routes>
       </BrowserRouter>
     </StreamVideoCallProvider>
